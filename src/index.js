358acfb841a0cb7ce6d212d9dd44d8076846d1d5
tanfouri <tanfouri.mohamed.marwan@gmail.com>
2023-10-17 14:20:42 +0100

    React-Router-Checkpoint

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 //import MovieDescription from './MovieDescription';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    
+<BrowserRouter>
+<App />
+</BrowserRouter>
   </React.StrictMode>
 );
 
